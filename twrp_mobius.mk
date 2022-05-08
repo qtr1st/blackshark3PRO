dc8f7244cda5cc04d09e32056dcf0c4fc002de1e
qtr1st <75439362+qtr1st@users.noreply.github.com>
2022-05-08 22:05:18 +0100

    Add files via upload

---
+++ twrp_mobius.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from mobius device
+$(call inherit-product, device/blackshark/mobius/device.mk)
+
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mobius
+PRODUCT_NAME := twrp_mobius
+PRODUCT_BRAND := blackshark
+PRODUCT_MODEL := SHARK MBU-H0
+PRODUCT_MANUFACTURER := blackshark
